0c5b1522334d341839f5136784f0f006f7a58e4b
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-03 08:11:50 +0530

    Add token generation for keeping the user signed in using jsonwebtoken
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- apps/server/src/routes/userRoute.ts
+++ apps/server/src/routes/userRoute.ts
@@ -2,13 +2,25 @@ import { Router } from "express";
 import { StatusCodes } from "../config/index"
 import { signupParameterValidation, signupInputValidation, doesUserNotExist } from "../middlewares/signup/index";
 import { createUser, encryptPassword } from "../utils/index";
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 const router = Router();
+const SECRET_KEY = process.env.JWT_SECRET;
 
 router.post("/signup",signupParameterValidation, signupInputValidation, doesUserNotExist, async (req, res)=>{
     const body = req.body;
     body.password = encryptPassword(body.password);
     
+    if(SECRET_KEY===undefined){
+        console.error("\n---------------------------------------------\nEnviroment Variable needs to have a JWT_SECRET\n---------------------------------------------\n")
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Internal server Error"
+        })
+    }
+
     const response = await createUser(body);
 
     if(!response.success){
@@ -17,9 +29,12 @@ router.post("/signup",signupParameterValidation, signupInputValidation, doesUser
         })
     }
 
+    const token = "Bearer " + jwt.sign({email: body.email, username: body.username}, SECRET_KEY, {expiresIn: '1 day'})
+
     return res.status(StatusCodes.CREATED).json({
-        message: response.message
+        message: response.message,
+        token: token
     })
 })
 
-export default router;
+export default router
