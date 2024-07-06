b692afac8e6fec01a0d862d4ab501bcb30bf8b49
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-06 17:34:04 +0530

    Integrate debouncing in every route
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- apps/server/src/utils/index.ts
+++ apps/server/src/utils/index.ts
@@ -2,12 +2,10 @@ import createUser from "./createUser";
 import encryptPassword from "./encryptPassword";
 import comparePassword from "../middlewares/signin/comparePassword";
 import logger from "./logger";
-import readLogs from "./readLogs";
 
 export {
     createUser,
     encryptPassword,
     comparePassword,
-    logger,
-    readLogs
+    logger
 }
