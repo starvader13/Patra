1761d2d79f7b77bc46e004c5e951c8cbf9b3f3a3
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-08 09:26:06 +0530

    Create LogIn Component
    - Add the username, email, and password fields to the LogIn component.
    - Improve the visual appeal of the signup form.
    - Added error message for the input field.
    
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- apps/client/src/pages/Login/Login.tsx
+++ apps/client/src/pages/Login/Login.tsx
@@ -1,8 +1,10 @@
+import { LogInForm, LogInGif } from "../../sections/Login"
 
 const Login = (): JSX.Element => {
-  return (
-    <div>Login</div>
-  )
+  return <div className="h-screen flex flex-col items-center md:flex-row md:justify-evenly md:items-center">
+    <LogInForm />
+    <LogInGif />
+</div>
 }
 
 export default Login
---
+++ apps/client/src/sections/Login/LogInGif.tsx
@@ -0,0 +1,8 @@
+
+const LogInGif = () => {
+    return <div className="md:hover:animate-pulse ">
+          <img src="/welcome.webp" alt="Login Patra" className="rounded-xl mb-4 mr-4"/>
+    </div>
+  }
+  
+  export default LogInGif;
