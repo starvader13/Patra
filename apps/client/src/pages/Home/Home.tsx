9eb672193b4de92d292f7065c9846a581d7642a7
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-09 12:58:44 +0530

    Add Card ID Section in the Home Page
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- apps/client/src/pages/Home/Home.tsx
+++ apps/client/src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import { CardIdSearch, Faq, Introduction, Testimonials } from "../../sections/Home"
 
 const Home = (): JSX.Element => {
-	return <div className="flex flex-col items-start justify-start p-6">
+	return <div className="flex flex-col items-start justify-start p-6 px-12">
 		<Introduction />
-		<div className="h-0.5 bg-sky-200 w-full"></div>
+		<div className="h-0.5 bg-slate-600 w-full"></div>
 		<CardIdSearch />
+		<div className="h-0.5 bg-slate-600 w-full"></div>
 		<Testimonials />
 		<Faq />
 	</div>
--- apps/client/src/sections/Home/Introduction.tsx
+++ apps/client/src/sections/Home/Introduction.tsx
@@ -1,7 +1,7 @@
 import IntroductionImage from "../../components/Home/IntroductionImage"
 
 const Introduction = () => {
-    return <div className="w-full flex justify-between items-center p-12 mt-4">
+    return <div className="w-full flex justify-between items-center py-12 mt-4 mb-8">
         <div className="flex flex-col gap-8 font-lato ml-6">
             <div className="text-4xl font-semibold text-zinc-200 text-opacity-90"><span className="text-red-400">Create.</span> Share. <span className="text-red-400">Impress.</span> Instantly</div>
             <div className="text-9xl font-medium pt-6">Patra </div>
